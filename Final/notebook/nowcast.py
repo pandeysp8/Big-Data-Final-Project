ed23fae50a80dae15d24a4decf585cf930d952a9
Swati Pandey <pandey.sw@husky.neu.edu>
2021-08-21 17:48:09 -0400

    change

--- Final/notebook/nowcast.py
+++
@@ -1,9 +0,0 @@
-# Load a part of the test dataset
-from readers.nowcast_reader import read_data
-import os
-
-os.environ["HDF5_USE_FILE_LOCKING"]='FALSE'
-import sys
-import numpy as np
-sys.path.append('../src/')
-x_test,y_test = read_data('../data/sample/nowcast_testing.h5',end=50)
